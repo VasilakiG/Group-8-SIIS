ed3436d12123f327006d674a076b7274d530d85f
darsov2 <62809499+darsov2@users.noreply.github.com>
2021-11-04 21:16:48 +0100

    Add files via upload

---
+++ lab1_1/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("a + b = %d\na - b = %d\na * b = %d\na / b = %d\na %% b = %d", a+b, a-b, a*b, a/b, a%b);
+    return 0;
+}
