34ef27b256a0954241ec99595162248be2dd62de
darsov2 <62809499+darsov2@users.noreply.github.com>
2021-12-08 10:32:05 +0100

    ro

--- Labs 6/lab6_1/main.c
+++ Labs 6/lab6_1/main.c
@@ -5,27 +5,36 @@ int main()
 {
     int m, n, br = 0;
     scanf("%d%d", &m, &n);
-    int a[m][n];
-    for(int i = 0; i < m; i++)
+    int a[m + 2][n + 2], b[m + 2][n + 2];
+    for(int i = 0; i < m + 2; i++)
     {
-        for(int j = 0; j < n; j++)
+        for(int j = 0; j < n + 2; j++)
         {
-            scanf("%d", &a[i][j]);
+            a[i][j] = -1;
+            b[i][j] = -1;
         }
     }
-    for(int i = 0; i < m; i++)
+
+    for(int i = 1; i < m + 1; i++)
     {
-        for(int j = 0; j < m; j++)
+        for(int j = 1; j < n + 1; j++)
+        {
+            scanf("%d", &a[i][j]);
+            b[i][j] = a[i][j];
+        }
+    }
+
+    for(int i = 1; i <= m; i++)
+    {
+        for(int j = 1; j <= n; j++)
         {
             br = 0;
             if(a[i][j] == 0)
             {
-
-                if(i >= 1 && j >= 1 && i < m-1 && j < n-1)
                 {
-                    for(int k = i-1; k < i+2; k++)
+                    for(int k = i - 1; k < i + 2; k++)
                     {
-                        for(int l = j - 1; l < j + 2; j++)
+                        for(int l = j - 1; l < j + 2; l++)
                         {
                             if(a[k][l] == 1)
                             {
@@ -34,17 +43,23 @@ int main()
                         }
                     }
                 }
+
+
+                b[i][j] = br;
             }
-            a[i][j] = br;
         }
 
     }
 
-    for(int i = 0; i < m; i++)
+    for(int i = 1; i < m + 1; i++)
     {
-        for(int j = 0; j < n; j++)
+        for(int j = 1; j < n + 1; j++)
         {
-            printf("%d", a[i][j]);
+            if(a[i][j] == 1)
+            {
+                printf("* ");
+            }
+            else printf("%d ", b[i][j]);
         }
         printf("\n");
     }
